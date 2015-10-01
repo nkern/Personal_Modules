acaf6dfe570d40a6a7032a6e3d948c13f2048a16
Nicholas S. Kern <nkern@umich.edu>
2015-10-01 02:25:31 -0700

    new file:   si_units.py

---
+++ si_units.py
@@ -0,0 +1,18 @@
+## SI Units list
+h_p		= 6.62607004e-34	#m2 kg / s
+k_b		= 1.38064852e-23	# m2 kg s-2 K-1
+G		= 6.67259e-11		# m3 kg-1 s-2
+c 		= 2.9979e8			# m s-1
+e_c		= 1.602e-19			# C
+m_p		= 1.67e-27			# kg
+m_e		= 9.109e-31			# kg
+sig_sb	= 5.670367e-8 		# W m−2 K−4
+Msun	= 1.981e30			# kg
+Rsun	= 6.955e8			# m
+Lsun	= 3.846e26 			# W
+
+
+
+
+
+
