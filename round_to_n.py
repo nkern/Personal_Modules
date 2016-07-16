2c5a0e25a305bf02995c63ba949116eafdd6972e
Nicholas S. Kern <nkern@umich.edu>
2016-07-16 01:43:52 -0700

    modified:   round_to_n.py

--- round_to_n.py
+++ round_to_n.py
@@ -5,4 +5,4 @@ def round_to_n(x,n=1):
 		return np.round(x, -int(np.floor(np.log10(x))) + (n - 1))
 	except:
 		shape = x.shape
-		return np.array(map(lambda z: np.round(z, -int(np.floor(np.log10(z))) + (n - 1)), x.ravel())).reshape(shape)
+		return np.array(map(lambda z: np.round(z, -int(np.floor(np.log10(np.abs(z)))) + (n - 1)), x.ravel())).reshape(shape)
