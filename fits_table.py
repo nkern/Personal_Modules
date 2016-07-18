3cc0a988cff351e42d746643889bf54c2b46e06a
Nicholas S. Kern <nkern@umich.edu>
2016-07-18 11:42:36 +0800

    modified:   fits_table.py

--- fits_table.py
+++ fits_table.py
@@ -42,7 +42,7 @@ def fits_data(fits_data,elim_zeros=True):
 
 def fits_to_array(fits_rec,elim_zeros=False):
 	dic = fits_data(fits_rec,elim_zeros=elim_zeros)
-	names = dic.names
+	names = fits_rec.names
 	grid = np.array( map(lambda x: grid[x], names) ).T
 	return grid
 
