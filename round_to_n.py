09312a33f5ba8fa03a4e78fb12f74d814db50c0d
Nicholas S. Kern <nkern@berkeley.edu>
2017-02-08 01:40:12 -0800

    new file:   biweight_midcovariance.py 	modified:   round_to_n.py

--- round_to_n.py
+++ round_to_n.py
@@ -1,8 +1,17 @@
+"""
+round_to_n.py
+-------------
+Adapted from:
+http://stackoverflow.com/questions/3410976/how-to-round-a-number-to-significant-figures-in-python
+
+Nicholas Kern
+2016
+"""
 import numpy as np
 
 def round_to_n(x,n=1):
 	try:
-		return np.round(x, -int(np.floor(np.log10(x))) + (n - 1))
-	except:
-		shape = x.shape
-		return np.array(map(lambda z: np.round(z, -int(np.floor(np.log10(np.abs(z)))) + (n - 1)), x.ravel())).reshape(shape)
+        return np.round(x, -int(np.floor(np.log10(x))) + (n - 1))
+    except:
+        shape = x.shape
+        return np.array(map(lambda z: np.round(z, -int(np.floor(np.log10(np.abs(z)))) + (n - 1)), x.ravel())).reshape(shape)
