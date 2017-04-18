a0ef85450dadf307e3d5ccc2a8410102c9f8ce0f
Nicholas S. Kern <nkern@berkeley.edu>
2017-04-18 13:34:33 -0700

    modified:   biweight_midcovariance.py

--- biweight_midcovariance.py
+++ biweight_midcovariance.py
@@ -1,30 +1,31 @@
 import numpy as np
 import astropy.stats as astats
 
-def biweight_midcovariance(a, c=9.0, M=None):
+def biweight_midcovariance(a, c=9.0, M=None, transpose=False):
     r"""
     Compute the biweight midcovariance.
-
     This is a robust and resistant estimator of the covariance matrix.
-
-    For more details, see:
-    `<http://www.itl.nist.gov/div898/software/
-    dataplot/refman2/auxillar/biwmidc.htm>_.`
 
     Parameters
     ----------
-    a : array-like, shape=(N_dims, N_samples)
-    Input array
+    a : array-like
+        A 2D numpy.ndarray of shape (N_variables, N_observations)
+        Each row of a represents a variable, and each column
+        a single observation of all variables (same as numpy.cov convention)
 
-    c : float, optional
-    Tuning constant. Default is 9.0
+    c : float, optional, default=9.0
+        Tuning constant.
 
     M : array-like, optional, shape=(N_dims,)
-    Initial guess for biweight location
+        Initial guess for biweight location
+
+    transpose : bool, optional, default=False
+        Transpose the input array
 
     Returns
     -------
     biweight_covariance : `~numpy.ndarray`
+        Estimate of the covariance matrix of <a, a.T>
 
     Examples
     --------
@@ -32,22 +33,42 @@ def biweight_midcovariance(a, c=9.0, M=None):
     numpy covariance and the biweight_covariance
 
     >>> import numpy as np
-    >>> import scipy.stats as stats
     >>> from astropy.stats import biweight_midcovariance
-    >>> d = np.array([stats.norm.rvs(0,1,1000),stats.norm.rvs(0,3,1000)])
-    >>> d[0,0] = 30
-    >>> np_cov = np.cov(d)
+    >>> # Generate 2D normal sampling of points
+    >>> rng = np.random.RandomState(1)
+    >>> d = np.array([rng.normal(0, 1, 200), rng.normal(0, 3, 200)])
+    >>> # Introduce an obvious outlier
+    >>> d[0,0] = 30.0
+    >>> # Calculate biweight covariances
     >>> bw_cov = biweight_midcovariance(d)
-    >>> print(np.sqrt(np_cov.diagonal()), np.sqrt(bw_cov.diagonal()))
-    (array([ 1.38868655,  2.97004256]), array([ 1.0232835 ,  3.02582728]))
+    >>> # Print out recovered standard deviations
+    >>> print(np.around(np.sqrt(bw_cov.diagonal()), 1))
+    [ 0.9  3.1]
 
     See Also
     --------
     biweight_midvariance, biweight_location
-    """
 
+    References
+    ----------
+    http://www.itl.nist.gov/div898/software/dataplot/refman2/auxillar/biwmidc.htm
+
+    """
+    # Ensure a is array-like
     a = np.asanyarray(a)
 
+    # Ensure a is 2D
+    if a.ndim != 2:
+        if a.ndim == 1:
+            a = a[np.newaxis,:]
+        else:
+            raise ValueError("a.ndim should equal 2")
+
+    # Transpose
+    if transpose == True and a.ndim == 2:
+        a = a.T
+
+    # Estimate location if not given
     if M is None:
         M = np.median(a, axis=1)
 
@@ -61,12 +82,17 @@ def biweight_midcovariance(a, c=9.0, M=None):
     # now remove the outlier points
     mask = np.abs(u) < 1
     n = mask.sum(axis=1)
-    usub1 = (1-u**2)
-    usub5 = (1-5*u**2)
+    usub1 = (1 - u ** 2)
+    usub5 = (1 - 5 * u ** 2)
     usub1[~mask] = 0.0
 
-    # now compute numerator and denominators
+    # now compute numerator and denominator
     numerator = d * usub1 ** 2
     denominator = (usub1 * usub5).sum(axis=1)[:, np.newaxis]
 
-    return n * np.dot(numerator, numerator.T) / np.dot(denominator, denominator.T)
+    # return estimate of the covariance
+    numerator_matrix = np.dot(numerator, numerator.T)
+    denominator_matrix = np.dot(denominator, denominator.T)
+    nsqrt = np.sqrt(n.reshape(1,-1))
+    return nsqrt * (numerator_matrix / denominator_matrix) * nsqrt.T
+
