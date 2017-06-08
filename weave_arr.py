e8098d7e325bae98c6ac3436d7b4df8f81552617
Nicholas S. Kern <nkern@berkeley.edu>
2017-06-08 11:04:41 -0700

    new file:   weave_arr.py

---
+++ weave_arr.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def weave(a, b):
+    """
+    weave two ndarrays 'a' and 'b' together
+    """
+    ashape = a.shape
+    bshape = b.shape
+    if len(ashape) == 1:
+        cshape = (ashape[0] + bshape[0],)
+    else:
+        cshape = (ashape[0]+bshape[0], ashape[1])
+    c = np.empty(cshape,dtype=a.dtype)
+    c[0::2] = a
+    c[1::2] = b
+    return c
+
