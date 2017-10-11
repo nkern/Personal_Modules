98428428e568c8caeae57d4c441202c795a6199b
Nicholas Kern <nkern@berkeley.edu>
2017-10-11 08:58:01 -0700

    new file:   hera_imports.py

---
+++ hera_imports.py
@@ -0,0 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.io.fits as fits
+from astropy.time import Time
+
+import hera_qm as hq
+import hera_cal as hc
+from pyuvdata import UVCal, UVData, UVFITS, UVBeam
+
