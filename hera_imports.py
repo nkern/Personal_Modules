d538b5d47cdd4db7accbb030874fcdd12b045fc2
Nicholas Kern <nkern@berkeley.edu>
2017-10-11 09:09:07 -0700

    modified:   hera_imports.py

--- hera_imports.py
+++ hera_imports.py
@@ -1,10 +1,75 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import astropy.io.fits as fits
-from astropy.time import Time
+# matplotlib
+try:
+    import matplotlib
+    import matplotlib.pyplot as plt
+except:
+    print('could not import matplotlib')
 
-import hera_qm as hq
-import hera_cal as hc
-from pyuvdata import UVCal, UVData, UVFITS, UVBeam
+# numpy
+import numpy as np
+
+# astropy
+try:
+    import astropy.io.fits as fits
+    from astropy.time import Time
+    import astropy.stats as astats
+except:
+    print('could not import astropy')
+
+# scipy
+try:
+    import scipy.stats as stats
+    import scipy.optimize as optimize
+    import scipy.signal as signal
+except:
+    print('could not import scipy')
+
+# pyuvdata
+try:
+    from pyuvdata import UVCal, UVData, UVFITS, UVBeam
+except:
+    print('could not import pyuvdata')
+
+# uvt
+try:
+    import uvtools as uvt
+except:
+    print('could not import uvtools')
+
+# heracal
+try:
+    import hera_cal as hc
+except:
+    print('could not import hera_cal')
+
+# hera qm
+try:
+    import hera_qm as hq
+except:
+    print('could not import hera_qm')
+
+# aipy
+try:
+    import aipy
+except:
+    print('could not import aipy')
+
+# other
+import glob
+import fnmatch
+import os
+import sys
+import subprocess
+import multiprocessing
+import cPickle
+import json
+import datetime
+import copy
+from collections import OrderedDict
+
+
+
+
+
+
 
